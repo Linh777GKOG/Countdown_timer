dd1c4a88ede14b68aba3685b8eb4894acbd872a7
Linh Vu <linh777gkog@gmail.com>
2022-06-24 22:52:01 +0700

    check time

---
+++ script.js
@@ -0,0 +1,9 @@
+const countToDate = new Date().setHours(new Date().getHours() + 24);
+let previousTimeBetweenDates;
+setInterval(() => {
+  const currentDate = new Date();
+  const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+  flipAllCards(timeBetweenDates);
+
+  previousTimeBetweenDates = timeBetweenDates;
+}, 250);
